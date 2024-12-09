38abdce7d493964a963f9102323c46888621aee3
BaBaTi07 <baptiste.moncomble@gmail.com>
2024-12-09 21:42:12 +0100

    ajout de l'annimation de feu

--- src/main/scala/station/displayEngine/Fire.scala
+++ src/main/scala/station/displayEngine/Fire.scala
@@ -6,42 +6,151 @@ class Fire extends Engine {
   this.op = fire_params
   val fop = fire_params
 
+  var flames: List[Flame] = List()
+  var smallFlames: List[Flame] = List()
+  var sparks: List[Spark] = List()
+
   def set_relative_val = {}
 
-  // ==========
+  // ========== 
   // | Engine |
   // ==========
 
   override def settings_pattern(): Unit = {
+    size(fop.width, fop.height)
+    if (fop.full_screen) fullScreen()
   }
 
   override def setup_pattern(): Unit = {
     set_relative_val
+    generate_flames()
+    generate_sparks()
   }
 
   override def draw_pattern(): Unit = {
-    draw_flame
+    background(fop.bg_color._1, fop.bg_color._2, fop.bg_color._3)
+    draw_flames()
+    draw_small_flames()
+    draw_sparks()
   }
 
   override def ajustement_pattern(): Unit = {
+    update_flames()
+    update_small_flames()
+    update_sparks()
   }
 
-  // =========
-  // | Flame |
-  // =========
+  // ========== 
+  // | Flames |
+  // ==========
 
-  def draw_flame = {
-    fill(0, 0, 0)
-    triangle(0, height, width, height, width/2, height/2)
+  case class Flame(var x: Float, var y: Float, var size: Float, var speed: Float)
+
+  def generate_flames(): Unit = {
+    flames = (0 until fop.flame_intensity).map { _ =>
+      Flame(random(width), height, random(50, 150), random(fop.flame_speed))
+    }.toList
+
+    smallFlames = (0 until fop.flame_intensity).map { _ =>
+      Flame(random(width), height, random(20, 70), random(fop.flame_speed))
+    }.toList
   }
 
+  def draw_flames(): Unit = {
+    fill(fop.flame_color._1, fop.flame_color._2, fop.flame_color._3, 150)
+    noStroke()
+    flames.foreach { flame =>
+      ellipse(flame.x, flame.y, flame.size, flame.size)
+    }
+  }
+
+  def draw_small_flames(): Unit = {
+    fill(fop.flame_color._1, fop.flame_color._2 - 50, fop.flame_color._3, 100)
+    noStroke()
+    smallFlames.foreach { flame =>
+      ellipse(flame.x, flame.y, flame.size, flame.size)
+    }
+  }
+
+  def update_flames(): Unit = {
+    flames = flames.map { flame =>
+      flame.y -= flame.speed
+      flame.size -= fop.flame_shrink_speed
+      if (flame.size <= 0) {
+        flame.y = height
+        flame.size = random(50, 150)
+      }
+      flame
+    }
+  }
+
+  def update_small_flames(): Unit = {
+    smallFlames = smallFlames.map { flame =>
+      flame.y -= flame.speed
+      flame.size -= fop.flame_shrink_speed
+      if (flame.size <= 0) {
+        flame.y = height
+        flame.size = random(20, 70)
+      }
+      flame
+    }
+  }
+
+  // ========== 
+  // | Sparks |
+  // ==========
+
+  case class Spark(var x: Float, var y: Float, var size: Float, var speed: Float)
+
+  def generate_sparks(): Unit = {
+    sparks = (0 until fop.spark_intensity).map { _ =>
+      Spark(random(width), height, random(5, 15), random(fop.spark_speed))
+    }.toList
+  }
+
+  def draw_sparks(): Unit = {
+    fill(fop.spark_color._1, fop.spark_color._2, fop.spark_color._3, 200)
+    noStroke()
+    sparks.foreach { spark =>
+      ellipse(spark.x, spark.y, spark.size, spark.size)
+    }
+  }
+
+  def update_sparks(): Unit = {
+    sparks = sparks.map { spark =>
+      spark.y -= spark.speed
+      spark.size -= fop.spark_shrink_speed
+      if (spark.size <= 0) {
+        spark.y = height - random(100)
+        spark.size = random(5, 15)
+      }
+      spark
+    }
+  }
 }
 
 object fire_params extends engine_param {
+  var flame_color: (Int, Int, Int) = (255, 100, 0)
+  var spark_color: (Int, Int, Int) = (255, 255, 0)
+  var flame_speed: Float = 2f
+  var spark_speed: Float = 5f
+  var flame_intensity: Int = 100
+  var spark_intensity: Int = 50
+  var flame_shrink_speed: Float = 0.5f
+  var spark_shrink_speed: Float = 0.1f
 }
 
 object Fire {
   def main(args: Array[String]): Unit = {
+    fire_params.full_screen = true
+    fire_params.bg_color = (0, 0, 0)
+    fire_params.flame_color = (255, 100, 0)
+    fire_params.spark_color = (255, 255, 0)
+    fire_params.flame_speed = 2f
+    fire_params.spark_speed = 5f
+    fire_params.flame_intensity = 100
+    fire_params.spark_intensity = 50
+
     PApplet.main(classOf[Fire].getName)
   }
 }
