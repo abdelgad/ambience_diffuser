cef5b3968d5b3cbd5afab61f136b70f8d5fbbfd7
Pozza Noé <noe.pozza@student.unamur.com>
2024-12-10 01:31:22 +0100

    fix and add begin musc

--- src/main/scala/station/algorithme/sensor_type.scala
+++ src/main/scala/station/algorithme/sensor_type.scala
@@ -1,26 +1,50 @@
 package station.algorithme
 
+import station.algorithme.Temperature.Temperature
+
 object Humidity extends Enumeration {
   type Humidity = Value
   val Sec, Moyen, Humide = Value
+  def norm(h:Humidity) = {
+    if (h == Sec) "Sec"
+    else if (h == Moyen) "Moyen"
+    else "Humide"
+  }
 }
 
 object Noise extends Enumeration {
   type Noise = Value
   val Normale, Bruyant = Value
+  def norm(n: Noise) = {
+    if (n == Normale) "Normale"
+    else "Bruyant"
+  }
 }
 
 object Illuminance extends Enumeration {
   type Illuminance = Value
   val Sombre, Claire = Value
+  def norm(i: Illuminance) = {
+    if (i == Sombre) "Sombre"
+    else "Claire"
+  }
 }
 
 object Temperature extends Enumeration {
   type Temperature = Value
   val Froid, Tempere, Chaud = Value
+  def norm(t: Temperature) = {
+    if (t == Froid) "Froid"
+    else if (t == Tempere) "Tempere"
+    else "Chaud"
+  }
 }
 
 object HeartRate extends Enumeration {
   type HeartRate = Value
   val Faible, Fort = Value
+  def norm(h: HeartRate) = {
+    if (h == Faible) "Faible"
+    else "Fort"
+  }
 }
--- src/main/scala/station/audio/SoundGenerator.scala
+++ src/main/scala/station/audio/SoundGenerator.scala
@@ -1,18 +1,18 @@
-//package station.audio
-//
-//import station.{Snapshot}
-//
-//object SoundGenerator {
-//
-//  def generateSound(snapshot: Snapshot): Unit = {
-//    val category = SoundDecisionTree.classifySnapshot(snapshot)
-//    val playlist = SoundDecisionTree.getPlaylist(category)
-//
-//    println(s"Generated Playlist for $category:")
-//    playlist.foreach { song =>
-//      println(s"- Playing: $song")
-//      AudioPlayer.play(s"src/main/resources/playlist/$song.mp3")
-//    }
-//  }
-//}
-//
+package station.audio
+
+import station.{Snapshot}
+
+object SoundGenerator {
+
+  def generateSound(snapshot: Snapshot): Unit = {
+    val category = SoundDecisionTree.classifySnapshot(snapshot)
+    val playlist = SoundDecisionTree.getPlaylist(category)
+
+    println(s"Generated Playlist for $category:")
+    playlist.foreach { song =>
+      println(s"- Playing: $song")
+      AudioPlayer.play(s"src/main/resources/playlist/$song.mp3")
+    }
+  }
+}
+
